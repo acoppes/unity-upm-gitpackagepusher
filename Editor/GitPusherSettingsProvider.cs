59003c9822141bbe37ddf71b5f42f5ca55a9f6f4
Ariel Coppes <ariel.coppes@gmail.com>
2020-04-12 15:22:29 -0300

    started adding preferences to control some behaviours

---
+++ Editor/GitPusherSettingsProvider.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEditor;
+
+namespace Gemserk.UPMGitPusher.Editor
+{
+    public static class GitPusherSettingsProvider
+    {
+        public const string PreferenceKeyDryRun = "UPMGitPusher.DryRun";
+        
+        [SettingsProvider]
+        public static SettingsProvider CreateMyCustomSettingsProvider()
+        {
+            
+            var provider = new SettingsProvider("Preferences/UPM Git Pusher", SettingsScope.User)
+            {
+                label = "UPM Git Pusher",
+                guiHandler = (searchContext) =>
+                {
+                    var dryRun = EditorPrefs.GetBool(PreferenceKeyDryRun, false);
+                    dryRun = EditorGUILayout.Toggle("Dry Run", dryRun);
+                    EditorPrefs.SetBool(PreferenceKeyDryRun, dryRun);
+                },
+                keywords = new HashSet<string>(new[] { "Git", "UPM" })
+            };
+
+            return provider;
+        }
+    }
+}
