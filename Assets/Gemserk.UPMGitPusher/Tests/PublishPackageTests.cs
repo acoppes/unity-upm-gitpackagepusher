c7543c0041981ab8cc4c6a8905ab5deec443e221
Ariel Coppes <ariel.coppes@gmail.com>
2020-04-16 21:01:05 -0300

    added tests project added test to avoid finding other files with package in name

---
+++ Assets/Gemserk.UPMGitPusher/Tests/PublishPackageTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Gemserk.UPMGitPusher.Editor;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class PublishPackageTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void TestPublishPackageShouldFindOnlyPackageJson()
+        {
+            try
+            {
+                var packageData = PublishVersionMenuItem.GetPackageData();
+            }
+            catch
+            {
+                Assert.Fail("Should not fail with exception");
+            }
+        }
+    }
+}
